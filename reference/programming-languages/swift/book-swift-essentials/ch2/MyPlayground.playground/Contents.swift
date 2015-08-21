f4f53da860f75430dc098a180c6dec03ac08a7de
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2015-08-20 21:59:03 -0500

    Adds new material to the reference folder
    It allows for more content in the reference folder. This time around it is about Swift Assets.

---
+++ reference/programming-languages/swift/book-swift-essentials/ch2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+for i in 1...12 {
+    var j = (i-7) * (i-6)
+    var k = i     println("I is \(i)")}
+let blue = UIColor.blueColor()
+let size = CGRect(x:0,y:0,width:200,height:100)
+let label = UILabel(frame:size)
+label.text = str
+label.textColor = blue
+label.font = UIFont.systemFontOfSize(24)
+let logo = UIImage(named:"logo")
+
+
+import XCPlaygroundXCPCaptureValue("logo",logo)
+
+for n in 1...6 {    if n % 2 == 0 {    XCPCaptureValue("even",n)    XCPCaptureValue("odd",0)} else {    XCPCaptureValue("odd",n)    XCPCaptureValue("even",0)    }}
